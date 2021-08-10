190750c5b4872ba16ac4cd63c8ea06bdb23f768e
Mr.Devid <devidtumoshenko@gmail.com>
2021-08-10 15:55:19 +0300

    Исправил страшную ошибку с коммитами, которая отобрала время

--- Project/cleaners/urls.py
+++ Project/cleaners/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, re_path
-
+from . import views
 from .views import CleanerList, CleanerDetail, CleanerCreation, CleanerUpdate, CleanerDelete
 
 
@@ -7,7 +7,7 @@ urlpatterns = [
 
     re_path(r'^$', CleanerList.as_view(), name='list'),
     re_path(r'^(?P<pk>\d+)$', CleanerDetail.as_view(), name='detail'),
-    re_path(r'^new$', CleanerCreation.as_view(), name='new'),
+    re_path('new', views.CleanerCreation),
     re_path(r'^edit/(?P<pk>\d+)$', CleanerUpdate.as_view(), name='edit'),
     re_path(r'^delete/(?P<pk>\d+)$', CleanerDelete.as_view(), name='delete'),
 
--- Project/cleaners/views.py
+++ Project/cleaners/views.py
@@ -1,7 +1,7 @@
-
+from django.shortcuts import render, redirect
 from django.urls import path, reverse_lazy
 from django.forms import ModelForm, TextInput, Textarea
-
+from .forms import CleanerForm
 
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
@@ -22,16 +22,27 @@ class CleanerDetail(DetailView):
     model = Cleaner
 
 
-class CleanerCreation(CreateView):
-    model = Cleaner
-    success_url = reverse_lazy('cleaners:list')
-    fields = ['first_name', 'last_name', 'quality_score', "city","other_city", "duration"]
-    widgets = {
-        'city': Textarea(attrs={
-            'class': "form-control",
-            'placeholder': 'Введите название',
-        })
+def CleanerCreation(request):
+    print(333333333333333)
+    cleaner = Cleaner.objects.all() # Получаем байтики данны из таблички CityList
+    error = '' # Шоб коду плохо не было создадим эту переменную заранее,а то ругается
+    if request.method == "POST": # Проверяем что мы там вообще получаем и если это было кинуто нам через POST (не церковный) - продолжаем
+        form = CleanerForm(request.POST)  # Запихаем эти данные в переменную
+        if form.is_valid(): # Проверяем на валидность (не напартачили ли со вводом)
+            form.save() # Кидаем всё это в нашу табличку в БД
+            #return redirect('/add_city')
+            error = "Форма заполнена не верно"
+
+    form = CleanerForm
+    # Ну тут мы просто пихаем наши байтики данных на страницу, где и уже сожно будет красивенько или не очень разместить
+    context = {
+        'form': form,
+        'error': error,
+        'cleaner': cleaner,
     }
+
+    return render(request, 'cleaners/cleaner_form.html', context)
+
 
 
 class CleanerUpdate(UpdateView):
