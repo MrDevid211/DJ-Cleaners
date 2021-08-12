153817c7437f47be53dc02adf196714bc1b310b0
Mr.Devid <devidtumoshenko@gmail.com>
2021-08-12 18:23:55 +0300

    Готовая форма для заказа клинера созданым юзером

--- Project/customers/models.py
+++ Project/customers/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Customer(models.Model):
+    id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=50)
